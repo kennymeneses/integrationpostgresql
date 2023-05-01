3ceacca162779b658bb9115c381a892cacb6b371
kennymeneses <kennny16m@gmail.com>
2023-05-01 13:06:35 -0500

    first commit

---
+++ DataAccess/BetDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class BetDbContext : DbContext
+    {
+        public BetDbContext(DbContextOptions<BetDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        public DbSet<Matches> Matches { get; set; }
+    }
+
+    public class Matches
+    {
+        public int Id { get; set; }
+        public string EquipoLocal { get; set; }
+        public string EquipoVisitante { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ResultadoLocal { get; set; }
+        public int ResultadoVisitante { get; set; }
+
+    }
+}
---
+++ DataAccess/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAccess
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task PostAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+    }
+}
